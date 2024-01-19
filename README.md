616336426e27e53e6df5ace3bfff5bf98e5c587d
Music Cat Bread <music.cat.bread@vvyy.xyz>
2024-01-19 11:28:11 +0100

    Some Fixes and new debug mode

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # Libvirt CPU Isolation Hook
 Isolate CPUs when launching Libvirt Virtual Machines.  
-For now this script is designed to run on non-SystemD systems and utilizes Control Groups (CGROUP) kernel feature.
+For now this script is designed to run on non-SystemD systems and utilizes Control Groups (CGROUPs) kernel feature.  
+#### NOTE: Because I am lazy. I didn't test this on anything else than OpenRC. So basically this is OpenRC CPU Isolation Hook with probably some kind of support for other init systems.
 
-### How to check if you have CGROUPs
-```
-$ zcat /proc/config.gz | grep 'CONFIG_CGROUPS=y' &>/dev/null; if [[ $? == 0 ]]; then echo OK; else echo No CGROUPs; fi
-```
-**or**
-```
-$ if [[ -d /sys/fs/cgroup ]]; then echo OK; else echo No CGROUPs; fi 
-```
+### Requirements
+ - Bash
+   ```
+   $ which bash
+   ```
+ - Kernel with CGROUPs support
+   ```
+   $ test $(zcat /proc/config.gz | grep 'CONFIG_CGROUPS=y') != "" && echo OK || echo No CGROUPs
+   ```
+   **or**
+   ```
+   $ test -d /sys/fs/cgroup && echo OK || echo No CGROUPs
+   ```
+ - Python >= 3.6
+   ```
+   $ python --version 
+   ```
+ - Preferably OpenRC as init system.
+   ```
+   test openrc && echo OK || echo Init not OpenRC   
+   ```
+   But others like DInit, SysVInit, S6 and RunIt should work perfectly fine.
 
-### Minimal python version
-You need python version **>=3.6**.  
-**Q: Why?**  
-Because bash is a little bit weird, this hook uses small python wrapper to spawn a new process. And this hook also needs to redirect all output of fork into /dev/null. And below python 3.6 it's done differently. Tbh on like 99% of linux systems you will have python version higher than this. So no need to worry about this and no need for me to do some weird cross python versions gymnastics.
+### Limitations
+ - Doesn't support running multiple isolated VMs at once **(and never will)**
+ - Currently you have to define which cores to leave for the system manually. Preferably in future this hook would simply read VM's XML file and check what cores we are left with.
 
 ### How to install:
 ```
@@ -26,6 +40,7 @@ $ mv vm-config $HOME/.config/ && chmod 444 $HOME/.config/vm-config
 # chown root:root /etc/libvirt/hooks/qemu.d/{hook.sh,username,wrapper.py}
 # chmod 555 /etc/libvirt/hooks/qemu.d/{hook.sh,username}
 # chmod 444 /etc/libvirt/hooks/qemu.d/wrapper.py
+$ cd .. && rm -r tmp_install_dir
 ```
 
 ### How to uninstall:
@@ -35,21 +50,37 @@ $ rm $HOME/.config/vm-config
 ```
 
 ### How to use
-Now that hook has been installed, open `$HOME/.config/vm-config` in your favorite text editor.  
-By default you will see two examples:
+Open `$HOME/.config/vm-config` in your favorite text editor.  
+Syntax is:
 ```
-gentoo.0,6
-gentoo_test.0,6,1,7
+VM_NAME.CPUs_FOR_HOST
 ```
-The general syntax is:
+To list all your VMs:
 ```
-YOUR_VM_NAME.CPUS_FOR_HOST
+$ virsh list --all
 ```
-`YOUR_VM_NAME` is VM name you chose when creating it (Labeled as `Name` in Virt Manager and virsh, NOT `Title`). After it follows a dot and CPUs you want to leave for your host os.
-For example take my Intel i7-9750H.
-
+if you don't see anything it might be that you have connected to wrong URI. Try those two:
 ```
-$ lscpu -e=CPU,CORE
+$ virsh -c qemu:///system list --all
+$ virsh -c qemu:///session list --all
+```
+CPU syntax is:
+`CPU` (thread) number, separated by `,` or range using `-`.  
+Examples:
+ - This will only leave threads `0` and `6` for host system and give Windows VM everything else.
+   ```
+   win10.0,6
+   ```
+ - This will give VM threads number `0` and `6` to VM and everything else remains for host.
+   ```
+   suicide_linux.1-5,7-11
+   ```
+To know which Cores (pairs of threads) to leave for your host use:
+```
+lscpu -e=CPU,CORE
+```
+Here's Intel i7-9750H in my Dell Precision 7740 laptop for reference.
+```
 CPU CORE
   0    0
   1    1
@@ -64,44 +95,62 @@ CPU CORE
  10    4
  11    5
 ```
-And as an exmaple let's say that I have following XML config in my gentoo VM
+NOTE: If this was and AMD system it would like more like this:
 ```
-  <cputune>
-    <vcpupin vcpu="0" cpuset="1"/>
-    <vcpupin vcpu="1" cpuset="7"/>
-    <vcpupin vcpu="2" cpuset="2"/>
-    <vcpupin vcpu="3" cpuset="8"/>
-    <vcpupin vcpu="4" cpuset="3"/>
-    <vcpupin vcpu="5" cpuset="9"/>
-    <vcpupin vcpu="6" cpuset="4"/>
-    <vcpupin vcpu="7" cpuset="10"/>
-    <vcpupin vcpu="8" cpuset="5"/>
-    <vcpupin vcpu="9" cpuset="11"/>
-  </cputune>
+CPU CORE
+  0    0
+  1    0
+  2    1
+  3    1
+  4    2
+  5    2
+  6    3
+  7    3
+  8    4
+  9    4
+ 10    5
+ 11    5
 ```
-XML here takes everything besides thread (labeled as `CPU` in `lscpu`) `0` and `6` for VM. So in that case our `vm-config` would look like this:
+QEMU/KVM will have mapping similar to AMD. So assuming we are following `win10.0,6` example, on Intel system your VM xml would look like this:
 ```
-gentoo.0,6
+<cputune>
+  <vcpupin vcpu="0" cpuset="1"/>
+  <vcpupin vcpu="1" cpuset="7"/>
+  <vcpupin vcpu="2" cpuset="2"/>
+  <vcpupin vcpu="3" cpuset="8"/>
+  <vcpupin vcpu="4" cpuset="3"/>
+  <vcpupin vcpu="5" cpuset="9"/>
+  <vcpupin vcpu="6" cpuset="4"/>
+  <vcpupin vcpu="7" cpuset="10"/>
+  <vcpupin vcpu="8" cpuset="5"/>
+  <vcpupin vcpu="9" cpuset="11"/>
+</cputune>
 ```
-Important note here about threads on systems with hyper threading (aka two threads per core).  
-**Intel** doesn't number their thread numbered next to each other. As you can see threads for core `0` are `0` and `6`.  
-But **AMD** has them numbered one after another. So for core `0`, thread numbers would be `0` and `1`.  
-My recommendation would be to always look at the output and decide which cores **(NOT THREADS)** you wan to leave for your system. And then write down corresponding thread numbers.
-
-### Limitations
- - Doesn't support running multiple isolated VMs at once.
- - Currently you have to define which cores to leave for the system manually. Preferably in future this hook would simply read VM's XML file.
+And on AMD system with `win10.0,1` VM more like this:
+```
+<cputune>
+  <vcpupin vcpu="0" cpuset="2"/>
+  <vcpupin vcpu="1" cpuset="3"/>
+  <vcpupin vcpu="2" cpuset="4"/>
+  <vcpupin vcpu="3" cpuset="5"/>
+  <vcpupin vcpu="4" cpuset="6"/>
+  <vcpupin vcpu="5" cpuset="7"/>
+  <vcpupin vcpu="6" cpuset="8"/>
+  <vcpupin vcpu="7" cpuset="9"/>
+  <vcpupin vcpu="8" cpuset="10"/>
+  <vcpupin vcpu="9" cpuset="11"/>
+</cputune>
+```
 
 ### Debugging
-If you have a problem with hook. Do the following:
- - Copy `hook.sh`, `wrapper.py` and `username` to a directory which you have read/write access (so you don't have to use sudo/doas over and over again)
- - Edit `hook.sh` and set SKIP_ROOT to true.
+If you want to debug then:
+ - Edit `hook.sh` and in config section set DEBUG to true
 
 Now hook will:
  - Print many things and don't actually try preform isolation
  - `/tmp/cpu_isolation_hook.lock` is owned by you
 
-Normally libvirt will call this script in one of three ways:  
+Normally libvirt will call this script in one of three ways (for testing purposes you can skip the dash, we never read it anyways):  
  - Starting
    ```
    bash /etc/libvirt/hooks/qemu.d/hook.sh VM_NAME prepare begin -
@@ -117,23 +166,26 @@ Normally libvirt will call this script in one of three ways:
 
 If you want to manually launch forked process that will run in background run:
 ```
-/usr/bin/python wrapper.py bash ./hook.sh PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS <VM_NAME> /tmp/cpu_isolation_hook.lock <CPUS_TO_LEAVE_FOR_HOST>
+/usr/bin/python wrapper.py bash ./hook.sh PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS <VM_NAME> <CPUS_TO_LEAVE_FOR_HOST>
 ```
-now python will return bash's PID, now write it to lock file
+now python wrapper will write bash's PID to stdout. Write it to lock file
 ```
 echo "VM_NAME.HOOK_PID" > /tmp/cpu_isolation_hook.lock
 ```
 If you want to kill that process. Use `kill -15 PID` (`15` is `SIGTERM`). It will do some cleanup and most importantly de-isolate CPUs when receiving this signal.  
-Also important note that this script uses
+
+Note that I have removed option to run this script without root, because it was quirky. And bugs arising from running in debug mode just kinda defeats it's purpose.  
+
+Also if you want to modify some code note that this hook uses:
 ```
 set -euo pipefail
 IFS=$'\n\t'
 ```
 which makes bash more stricter.  
-Read more about it [here](http://redsymbol.net/articles/unofficial-bash-strict-mode/) to learn about it and some workaround for common things which are done differently.
+Read about it [here](http://redsymbol.net/articles/unofficial-bash-strict-mode/) to learn more and some workarounds for common things which are done differently.
 
 ### Contributing
-Just open an issue or pull request. Or report that you had success running this on init systems other than SystemD/OpenRC.
+Just open an issue or pull request. Or report that you had success running this on your system.
 
 ### License
 This project is licensed under **MIT License**. See `LICENSE` file for full legal text.
--- hook.sh
+++ hook.sh
@@ -3,32 +3,27 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# NOTE: Config is on line 57
-
 #                            CPU Isolation hook
 #                           For r/VFIO community
 #                           and all linux gamers
 #
 #                              !! WARNING !!
-# This script was designed to run on linux distributions NOT running SystemD,
-# and with CGroups support compiled in kernel. Following init systems are
-# theoretically* supported like: DInit, RunIt, SySVInit, s6 and OpenRC.
+# This script was designed to utilize Contro Groups (CGROUPs) Kernel feature.
+# Because SystemD manages CGROUPs, it's not supported (for now). And it's only
+# been tested on OpenRC (because I can't be bothered to test anything else), but
+# inits like DInit, SysVInit, S6 or RunIt should in theory work perfectly fine
+# with this script.
 #
 # While there's SYSTEMD_FAIL_OVERWRITE to bypass systemd check, I advise against
-# using it. Under SystemD most of CGroups are managed by systemctl utility and
-# this big, multi process hook can be reduced to 6 commands. 3 to pin CPUs, and
-# another 3 to un-pin.
+# using it. You really should just use systemctl.
 #
-# I AM NOT RESPONSIBLE FOR ANY ACTION RESULTED FROM RUNNING THIS SCRIPT, NOR YOU
-# ARE ENTITLED TO BITCH ABOUT LOOSING RANDOM CODE THAT YOU COPY PASTED FROM
-# STACK OVERFLOW, BUT LOST BECAUSE YOUR SYSTEM FROZE. AND YES I AM GOING TO
-# SWEAR BECAUSE NOBODY IS EVER READING WARNINGS OR ANY KIND OF "NOT RESPONSIBLE"
-# PARAGRAPHS LOL
-#
-# * I don't have time to test this script on all of those init systems. I only
-#   use OpenRC, but in theory this script is init independent on the condition
-#   that such init system doesn't screw around with CGroups (I am looking at
-#   you, systemd)
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # TODO: If should be easy to just use some other set of commands in case of
 #       systemd. Maybe I will add systemd support in future. Actually that would
@@ -42,9 +37,10 @@ IFS=$'\n\t'
 
 # Check username file as it's needed to be checked before config
 HOOK_FILE="$(which $0)"
-cd $(dirname $HOOK_FILE)
-if [[ ! -f './username' ]];
-    then if [[ -d './username' ]]; then
+HOOK_DIR=$(dirname $HOOK_FILE)
+cd $HOOK_DIR
+if [[ ! -f './username' ]]; then
+    if [[ -d './username' ]]; then
         echo "$HOOK_FILE: ERROR: './username' is a directory, not a file."
         exit 0
     else
@@ -57,197 +53,165 @@ fi
 #  ~*~  Config  ~*~
 # Location of file containing VM names and CPUs
 CONFIG_FILE=/home/$(cat username)/.config/vm-config
-# CGroup directory
-CGROUP_DIR='/sys/fs/cgroup'
-# Set to true if you want to bypass systemd check
-SYSTEMD_FAIL_OVERWRITE=false
-# Skip checking if kernel is compiled with CGROUPs support
-SKIP_CGROUP_CHECK=false
-# Lock file location, don't change unless nesesery
-LOCK_FILE='/tmp/cpu_isolation_hook.lock'
 # What python interpreter to use, version >= 3.6
 PYTHON_BINARY='/usr/bin/python'
-# Don't use this. It skips some code so it can be tested without requiring me
-# to re-type my password every few minutes.
-SKIP_ROOT=false
+# Lock file location, don't change unless nesesery
+LOCK_FILE='/tmp/cpu_isolation_hook.lock'
+# Set to true if you want to bypass systemd check
+SYSTEMD_FAIL_OVERWRITE=false
+# CGroup directory
+CGROUP_DIR='/sys/fs/cgroup'
+# Debugging, don't enable or else libvirt will spam you with "errors"
+DEBUG=true
 #  ~*~  Config END  ~*~
 
 
+# Print only if DEBUG == true.
+deb () {
+    if [[ ! -z ${1:-} ]] && [[ $DEBUG == true ]]; then
+        echo $1
+    fi
+}
+
 # Don't allow script to be re-run if we crash
 make_lock_file_dirty () {
-    # At this point we don't care about being easy to debug, the only thing we
-    # have to do is mark lock file as dirty. And crashing without any signs of
-    # failure would be VERY annoying
+    # We have to execute everything in this function, even if something fails.
     set +euo pipefail
+
+    deb "MAKE LOCK FILE DIRTY"
 
     # Read lock file data (if any)
     LOCK_DATA=$(cat $LOCK_FILE)
     
+    deb "LOCK DATA: $LOCK_DATA"
+
     # Mark lock file as dirty
     if [[ $LOCK_DATA != "" ]]; then
         echo "$(echo $LOCK_DATA | awk -F '.' '{print $1}').-1"
+        deb "GOOD LOCK FILE WRITE"
     else
         echo "ERROR_PHRASING_VM_NAME.-1" > $LOCK_FILE
+        deb "LOCK FILE INCORRECT VM NAME"
     fi
     chmod 644 $LOCK_FILE
-    if [[ $SKIP_ROOT == false ]]; then
-        chown root:root $LOCK_FILE
-    fi
+    chown root:root $LOCK_FILE
 
     # Allow every process to run on every thread
     CPUs=$(find $CGROUP_DIR -maxdepth 2 -type f -name 'cpuset.cpus' -not -path '*/machine/*')
     for i in $CPUs; do
-        if [[ $SKIP_ROOT == false ]]; then
-            echo $(lscpu | grep 'On-line CPU(s) list:' | grep -oE '[0-9]*-[0-9]*$') > $i
-        else
-            echo "echo \"$(lscpu | grep 'On-line CPU(s) list:' | grep -oE '[0-9]*-[0-9]*$')\" > $i"
-        fi
+        $LINE_CPUs=$(lscpu | grep 'On-line CPU(s) list:' | grep -oE '[0-9]*-[0-9]*$')
+        echo $LINE_CPUs > $i
+        deb "echo \"$LINE_CPUs\" > $i"
     done
 
-    # Disable CPU set for CGROUPs.
-    # In most cases this will fail. Because "device or resource is busy".
-    # *shrug*
-    if [[ $SKIP_ROOT == false ]]; then
-        echo "-cpuset" > "$CGROUP_DIR/cgroup.subtree_control"
-    else
-        echo "echo \"-cpuset\" > \"$CGROUP_DIR/cgroup.subtree_control\""
-    fi
-
-    # If for some reason we are not actually forked. Exit with code 1
+    # Exit with code 1. While debugging will be useful.
     exit 1
 }
 
 # De-isolate CPUs and remove lock file
 exit_watcher () {
+    # Get rid of the lock file
+    rm -f $LOCK_FILE
+
+    # Nothing to do
+    if [[ -z ${1:-} ]]; then
+        deb "EXIT WATCHER EMPTY \$1"
+        exit 0
+    fi
+
     # We don't care if we fail at this stage
     set +euo pipefail
 
-    # Debug
-    if [[ $SKIP_ROOT == true ]]; then
-        echo 'RECEIVED SIGTERM'
-    fi
-
-    # We got killed really early. Because we didn't redefine trap with new
-    # arguments. Don't even bother doing anything.
-    if [[ -z ${1:-} ]]; then
-        exit 0
-    fi
+    deb "RECEIVED SIGTERM"
+    deb "TRAP DATA: $1"
 
     # De-isolate everything
     IFS=!
     for i in $1; do
+        deb "$i"
         LAST_STATE=$(echo $i | awk -F '~' '{print $2}')
         if [[ $LAST_STATE == "" ]]; then
+            deb "NO LAST STATE"
             VALUE=$(lscpu | grep 'On-line CPU(s) list:' | grep -oE '[0-9]*-[0-9]*$')
         else
             VALUE=$(echo $i | awk -F '~' '{print $2}')
+            deb "STATE FOUND: $VALUE"
         fi
+        SET_DIR=$(echo $i | awk -F '~' '{print $1}')
 
-        if [[ $SKIP_ROOT == false ]]; then
-            echo "$LAST_STATE" > $(echo $i | awk -F '~' '{print $1}')
-        else
-            echo "echo \"$(echo $i | awk -F '~' '{print $2}')\" > $(echo $i | awk -F '~' '{print $1}')"
-        fi
+        echo "$VALUE" > $SET_DIR
+        deb "echo \"$VALUE\" > $SET_DIR"
     done
     IFS=$'\n\t'
-
-    # Disable CPU set for CGROUPs.
-    # In most cases this will fail. Because "device or resource is busy".
-    # *shrug* 
-    if [[ $SKIP_ROOT == false ]]; then
-        echo "-cpuset" > "$CGROUP_DIR/cgroup.subtree_control"
-    else
-        echo "echo \"-cpuset\" > \"$CGROUP_DIR/cgroup.subtree_control\""
-    fi
-
-    # Get rid of the lock file
-    rm -f $LOCK_FILE
 
     # This is required. Or we will continue watching and de-isolating forever.
     exit 0
 }
 
-# Watch for changes. Main logic for forked process
+# Watch for changes. Main debic for forked process
 watcher () {
-    # Debug
-    if [[ $SKIP_ROOT == true ]]; then
-        echo HOOK with PID $$
-        echo "\$1: $1"
-        echo "\$2: $2"
-        echo "\$3: $3"
-    fi
+    deb "HOOK with PID $$"
+    deb "\$1: $1"
+    deb "\$2: $2"
 
-    # Catch SIGTERM (kill -15 $PID) and call exit function Note that this call
-    # is not finished and will be updates with saved CGROUPs state later as an
-    # argument
+    # Catch SIGTERM (kill -15 $PID)
     trap "exit_watcher" SIGTERM
-
-    # If we crash at this point we want to mark this hook as failed. Because we
-    # forked of, we don't posses any control over vm status. And randomly
-    # calling virsh to shut it down is not the best idea. So we just set PID in
-    # lock file as -1 and on next launch, user will be informed of the error.
+    # If crash mark lock file as dirty
     trap 'make_lock_file_dirty' ERR
 
-    # This workaround is absolutely stupid
-    # So when we fork and another process tries to kill us, sleep command will
-    # block trap and we will have to wait sometimes like 5 seconds. So we sleep
-    # in smaller increments. That doesn't overload the CPU by non-stop reading
-    # /sys/fs and also makes script responsive to being killed. For fuck sake,
-    # why.
+    # Sleep in intervals so sleep doesn't block SIGTERM
     SLEEP_TIME=5
     SLEEP_INTERVAL=0.2
     SLEEP_COUNTER=$(echo "$SLEEP_TIME $SLEEP_INTERVAL" | awk '{print $1 / $2}')
+    deb "SLEEP: $SLEEP_TIME = $SLEEP_INTERVAL * $SLEEP_COUNTER"
 
-    # Here we will store CGROUPs with their last known states before starting
-    # isolating.
+    # CGROUPs with their last know state
     declare -A CGROUPs
 
-    # Enable CPU set for CGROUPs
-    if [[ $SKIP_ROOT == false ]]; then
-        echo "+cpuset" > "$CGROUP_DIR/cgroup.subtree_control"
-    else
-        echo "echo \"+cpuset\" > \"$CGROUP_DIR/cgroup.subtree_control\""
-    fi
-
-    # Now we watch till we receive kill -15 (aka SIGTERM)
+    # This will go on forever until we receive SIGTERM
+    deb "HOOK LOOP"
     while true; do
-        # Libvirt creates a new cgroup called `machine`. We don't want to limit
-        # it because libvirt needs to have full control over itself in order to
-        # use correct CPU threads
+        deb "LOOP CYCLE BEGIN"
+
+        # Libvirt creates a new cgroup called `machine`. Wo don't want to mess
+        # with it
         CPUs=$(find $CGROUP_DIR -maxdepth 2 -type f -name 'cpuset.cpus' -not -path '*/machine/*')
+        UPDATE=false
         for i in $CPUs; do
-            # Check if we don't have that cgroup save it's value
+            SYS_FS_VALUE=$(cat $i)
+
+            # Check if we have CGROUP's value
             if [[ -z ${CGROUPs[$i]:-} ]]; then
-                CGROUPs[$i]=$(cat $i)
+                CGROUPs[$i]=$SYS_FS_VALUE
+                deb "$i = $SYS_FS_VALUE"
             fi
 
             # If we don't have desired isolation, change it
-            if [[ $(cat $i) != $3 ]]; then
-                if [[ $SKIP_ROOT == false ]]; then
-                    echo $3 > $i
-                else
-                    # NOTE: Because we actually never write when SKIP_ROOT is
-                    # true. Every time we print this. In reality we would only
-                    # do it when the value doesn't match up.
-                    echo "echo $3 > $i"
-                fi
+            if [[ $SYS_FS_VALUE != $2 ]]; then
+                echo $2 > $i
+                UPDATE=true
+                deb "UPDATE $i $2 -> $SYS_FS_VALUE"
             fi
         done
 
-        # In case we get asked to stop, we call wrapper_exit.
-        # But it needs to restore states, so we need to pass last
-        # known states to it. Here we redefine function argument.
-        NEW_TRAP=""
-        for i in "${!CGROUPs[@]}"; do
-            NEW_TRAP+="$i~${CGROUPs[$i]}!"
-        done
-        if [[ $NEW_TRAP != "" ]]; then
-            NEW_TRAP=${NEW_TRAP::-1}
+        # In case we get asked to stop, we call wrapper_exit. But it needs to
+        # restore states, so we need to pass last known states to it. Here we
+        # redefine trap argument.
+        if [[ $UPDATE == true ]]; then
+            NEW_TRAP=""
+            for i in "${!CGROUPs[@]}"; do
+                NEW_TRAP+="$i~${CGROUPs[$i]}!"
+            done
+            if [[ $NEW_TRAP != "" ]]; then
+                NEW_TRAP=${NEW_TRAP::-1}
+            fi
+            trap "exit_watcher $NEW_TRAP" SIGTERM
+            deb "NEW TRAP $NEW_TRAP"
         fi
-        trap "exit_watcher $NEW_TRAP" SIGTERM
 
         # Sleep for given amount of time.
         # Look at $SLEEP_COUNTER definition above for explanation of this mess.
+        deb SLEEP
         for i in $(seq $SLEEP_COUNTER); do
             sleep $SLEEP_INTERVAL;
         done
@@ -262,27 +226,8 @@ wrapper () {
         exit 0
     fi
 
-    # Check if system is compiled with support for CGROUPs
-    if [[ $SKIP_CGROUP_CHECK == false ]]; then
-        set +euo pipefail
-        OUTPUT=$(zcat /proc/config.gz | grep 'CONFIG_CGROUPS=y')
-        EXIT_CODE=$?
-        set -euo pipefail
-        if [[ $(echo $OUTPUT | wc -l) == 0 ]] || [[ $EXIT_CODE != 0 ]]; then
-            echo "$HOOK_FILE: ERROR: Kernel compiled without CGROUPs support"
-            exit 0
-        fi
-    fi
-
-    # Check if host machine is running SystemD
-    # This is official method to check for SystemD
-    if  [[ -d '/run/systemd/system' && $SYSTEMD_FAIL_OVERWRITE == false ]]; then
-        echo "$HOOK_FILE: ERROR: SystemD detected!"
-        exit 0
-    fi
-
     # Check if we are root
-    if [[ "$EUID" != 0 && ! $SKIP_ROOT ]]; then
+    if [[ "$EUID" != 0 ]]; then
         echo "$HOOK_FILE: ERROR: This hook requires root permissions"
         exit 0
     fi
@@ -293,19 +238,8 @@ wrapper () {
         if [[ -d $CONFIG_FILE ]]; then
             echo "$HOOK_FILE: ERROR: $CONFIG_FILE is a directory"
             exit 0
-        else # Create if it doesn't exist
-            touch $CONFIG_FILE
-            if [[ $SKIP_ROOT == false ]]; then
-                chown root:libvirt $CONFIG_FILE
-            fi
-            chmod 660 $CONFIG_FILE
-            echo "# Below is example config
-# NOTE: This file is owned by libvirt
-#       group. So if you are in libvirt
-#       group you don't have to use sudo/
-#       doas in order to edit this file.
-example_vm_name.0,6,1-2" > $CONFIG_FILE
-            echo "$HOOK_FILE: ERROR: $CONFIG_FILE didn't exist. It's been created. Please now edit it."
+        else # Nothing :(
+            echo "$HOOK_FILE: ERROR: $CONFIG_FILE doesn't exist."
             exit 0
         fi
     fi
@@ -323,13 +257,18 @@ example_vm_name.0,6,1-2" > $CONFIG_FILE
 
     # No config for this vm found, ignoring call to hook
     if [[ $HOST_CPU == 'NOTHING_FOUND' ]]; then
+        deb "NO CONFIG FOUND FOR VM: $1"
         exit 0
     fi
 
     # If we are starting or restoring VM
     if [[ "$2.$3" == "prepare.begin" || "$2.$3" == "restore.begin" ]]; then
+        deb "BEGIN: $2.$3"
+
         # Handle the lock file
         if [[ -f $LOCK_FILE ]]; then
+            deb "$LOCK_FILE EXISTS"
+
             set +euo pipefail
             INFO=$(cat $LOCK_FILE)
             if [[ $(echo $INFO | awk -F '.' '{print $2}') == -1 ]]; then
@@ -343,31 +282,36 @@ example_vm_name.0,6,1-2" > $CONFIG_FILE
             exit 0
         fi
 
-        # Prepare lock file
-        touch $LOCK_FILE
-        chmod 644 $LOCK_FILE
-        if [[ $SKIP_ROOT == false ]]; then
-            chown root:root $LOCK_FILE
-        fi
-
         # Fork new process. It will change isolation and keep watching for new
         # CGROUPs. Thanks to reddit.com/u/Academic_Yogurt966 for finding that
         # gentoo's portage creates new cgroup.
-        PID=$($PYTHON_BINARY wrapper.py bash $HOOK_FILE PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS $1 $LOCK_FILE $HOST_CPU)
+        deb "CALLING:"
+        deb "$PYTHON_BINARY $HOOK_DIR/wrapper.py /bin/bash $HOOK_FILE PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS $1 $HOST_CPU"
+        PID=$($PYTHON_BINARY $HOOK_DIR/wrapper.py /bin/bash $HOOK_FILE PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS $1 $HOST_CPU)
 
         # Write data to lock file
+        touch $LOCK_FILE
+        chmod 644 $LOCK_FILE
+        chown root:root $LOCK_FILE
         echo "$1.$PID" > $LOCK_FILE
+        deb "$LOCK_FILE: "$1.$PID""
 
         # Exit
         exit 0
     elif [[ "$2.$3" == "release.end" ]] && [[ -f $LOCK_FILE ]]; then
+        deb "RELEASE END"
+
+        # We are ending but lock file is dirty. Idk how you would get into this
+        # position because you can't run two hooks at the same time but idk.
         if [[ $(cat $LOCK_FILE | awk -F '.' '{print $3}') == "SIGKILL" ]]; then
+            deb "LOCK FILE ALREADY DIRTY, IGNORING RELEASE END"
             exit 0
         fi
 
         # Ask our hook to exit
         # It needs to clear all cpuset.cpus files
         HOOK_PID=$(cat $LOCK_FILE | awk -F '.' '{print $2}')
+        deb "HOOK PID: $HOOK_PID"
         set +euo pipefail
         kill -15 $HOOK_PID
         timeout 5 tail --pid=$HOOK_PID -f /dev/null
@@ -384,15 +328,20 @@ example_vm_name.0,6,1-2" > $CONFIG_FILE
     fi
 }
 
-# Check if we have first argument
-# Naive trap to stop anyone from running this script directly
-if [ ! -n "$1" ]; then
-    echo "$HOOK_FILE: ERROR: No arguments. This script is supposed to be run as QEMU Hook."
-    exit 0
-elif [[ $1 == 'PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS' ]]; then
-    watcher $2 $3 $4
+# Check if we have enough variables
+if [[ -z ${1:-} || -z ${2:-} || -z ${3:-} ]]; then
+    echo "$HOOK_FILE: ERROR: Not enough arguments."
     exit 0
 else
-    wrapper $1 $2 $3
-    exit 0
+    if [[ $1 == 'PLEASE_FOR_FUCKS_SAKE_DO_NOT_NAME_YOUR_VM_LIKE_THIS' ]]; then
+        deb "WATCHER CALL"
+        watcher $2 $3
+        deb "WATCHER END NO DERIVE"
+        exit 0
+    else
+        deb "WRAPPER CALL"
+        wrapper $1 $2 $3
+        deb "WRAPPER END"
+        exit 0
+    fi
 fi
